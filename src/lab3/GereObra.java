4363121d937b7a4df3b84dce243759f447ccab19
Utilizador <Utilizador@DESKTOP-GQGV52P.lan>
2025-09-21 19:08:50 +0100

    Imprimir o título e o autor das pinturas

--- src/lab3/GereObra.java
+++ src/lab3/GereObra.java
@@ -42,5 +42,15 @@ public class GereObra {
 	    }
 	}
 	
+	public void imprimirTituloAutor(String tela) {
+        for (Obra o : obras) {
+            if (o instanceof PintoOleo) {
+                PintoOleo po = (PintoOleo) o; 
+                if (po.getTipoTela().equalsIgnoreCase(tela)) {
+                    System.out.println("Título: " + po.getTitulo() + " | Autor: " + po.getAutor());
+                }
+            }
+        }
+    }
 }
 
